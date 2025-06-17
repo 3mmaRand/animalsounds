a64fd1b0f7f07ef064e05471db2aa05796b37d52
Emma Rand <emma.rand@york.ac.uk>
2025-06-17 12:52:43 +0100

    document animal_sounds() function

--- R/animalsounds.R
+++ R/animalsounds.R
@@ -1,3 +1,15 @@
+#' Print a sentence saying what sound an animal makes from user inputs
+#'
+#' @param animal input from the user
+#' @param sound input from the user
+#'
+#' @returns a string that prints "the {animal} goes {sound}, {sound}!"
+#' @export
+#'
+#' @examples
+#' animal_sounds("dog", "woof")
+#' animal_sounds("cat", "meow")
+
 animal_sounds <- function(animal, sound) {
   stopifnot(is.character(animal), length(animal) == 1)
   stopifnot(is.character(sound), length(sound) == 1)
